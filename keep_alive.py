6c59988c957c8b396f51112ae049ffddf7e634e5
GLHF-Goose <153947807+GLHF-Goose@users.noreply.github.com>
2025-05-02 21:07:38 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,9 +9,12 @@ def home():
     return "Hello. I am alive!"
 
 def run():
+    print("Running on port:")
     print(os.environ['PORT'])
     app.run(host='0.0.0.0',port=os.environ['PORT'])
 
 def keep_alive():
+    print("Keeping alive")
     t = Thread(target=run)
     t.start()
+    print("Started thread")
